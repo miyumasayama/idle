0dccf129b7f7c4ff014508f510d786454ae94f3d
JoNaKagwa <nakagawajo0510@gmail.com>
2020-10-09 03:36:42 +0900

    add_test_factory

---
+++ spec/factories/profile.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :profile do    
+    user
+
+    nickname { "有村架純" }
+    age { 22 }
+    favorite { "菅田将暉" }
+    introduce { "test" }
+    created_at { 2020-05-10 }
+    updated_at { 2020-05-10 }
+    sex { 1 }
+  end
+end
