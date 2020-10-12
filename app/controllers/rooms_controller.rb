23a0fb05f8a3e8afcdf98a8ac79a0f8670cd50e4
miyumasayama <921ohagi@gmail.com>
2020-10-13 02:26:40 +0900

    massage on the way

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,16 +1,22 @@
 class RoomsController < ApplicationController
     before_action :require_login
     def index
-      @user = User.find(id:receiver_id)
       @rooms = Room.where(user_id:current_user.id).or(Room.where(receiver_id: current_user.id))
-      
+      @rooms.each do |r|
+        if r.user_id == current_user.id
+          @user = User.find_by(:id => r.receiver_id)
+        else
+          @user = User.find_by(:id => r.user_id)
+        end
+      end
     end
 
     def create
       @user = User.find_by(params[:room][:receiver_id])
       @room = Room.new(user_id:current_user.id, receiver_id: @user.id)
-      @room.save
-      redirect_to room_path(@room.id)
+      if @room.save
+        redirect_to room_path(@room.id)
+      end
     end
 
     def show 
