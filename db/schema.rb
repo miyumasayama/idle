860d1c6e289c4e1cdd4451f8ba2861e2b6cc8d90
miyumasayama <921ohagi@gmail.com>
2020-10-21 14:35:41 +0900

    add limit to email

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_11_165949) do
+ActiveRecord::Schema.define(version: 2020_10_21_052749) do
 
   create_table "comments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "content"
@@ -62,7 +62,7 @@ ActiveRecord::Schema.define(version: 2020_10_11_165949) do
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "email"
+    t.string "email", limit: 191
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
