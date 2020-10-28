5fd4d57b705e21f65c57b7c9d0c9bd322a2d9d3a
miyumasayama <921ohagi@gmail.com>
2020-10-28 13:49:29 +0900

    fixed functions

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,4 +2,10 @@
 var componentRequireContext = require.context("components", true);
 var ReactRailsUJS = require("react_ujs");
 ReactRailsUJS.useContext(componentRequireContext);
-require('jquery')
+require("@rails/ujs").start()
+require("@rails/activestorage").start()
+require("channels")
+require("jquery")
+
+import 'bootstrap'
+import '../src/application.scss'
